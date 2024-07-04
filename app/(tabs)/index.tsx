1c49b2445f5545811060ad3e98e7070971706bc0
Alexandr Ziborov <alexandr@getta.io>
2024-07-04 22:44:36 +0300

    Implement first screen text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,34 +16,27 @@ export default function HomeScreen() {
                 />
             }>
             <ThemedView style={styles.titleContainer}>
-                <ThemedText type="title">Welcome!</ThemedText>
+                <ThemedText type="title">Welcome SPA comments!</ThemedText>
                 <HelloWave/>
             </ThemedView>
             <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+                <ThemedText type="subtitle">Step 1: Select user</ThemedText>
                 <ThemedText>
-                    Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-                    Press{' '}
-                    <ThemedText type="defaultSemiBold">
-                        {Platform.select({ios: 'cmd + d', android: 'cmd + m'})}
-                    </ThemedText>{' '}
-                    to open developer tools.
+                    Press <ThemedText type="defaultSemiBold">Explore</ThemedText>.
+                    Explore user's events.
                 </ThemedText>
             </ThemedView>
             <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+                <ThemedText type="subtitle">Step 2: Read users events. </ThemedText>
                 <ThemedText>
-                    Tap the Explore tab to learn more about what's included in this starter app.
+                    You can list users events and read every event.
                 </ThemedText>
             </ThemedView>
             <ThemedView style={styles.stepContainer}>
-                <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+                <ThemedText type="subtitle">Step 3: Add comments to user's event.</ThemedText>
                 <ThemedText>
-                    When you're ready, run{' '}
-                    <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-                    <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-                    <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-                    <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+                    When you're ready, add{' '}!
+
                 </ThemedText>
             </ThemedView>
         </ParallaxScrollView>
