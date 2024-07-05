3cbdeb1d02441e7a4b1f63130fbf2da05fdddc37
Alexandr Ziborov <alexandr@getta.io>
2024-07-05 11:11:22 +0300

    Add db images

--- constants/Database.ts
+++ constants/Database.ts
@@ -1,13 +1,28 @@
 const fs = require('fs');
 
-
 export const DATABASE = {
 
     users: [
 
-            {
-                name: "Antonym",
-            }
+        {
+
+            id: 1,
+
+            name: "Antonym",
+
+            avatarName: "./assets/images/user/user01.png"
+
+        },
+        {
+
+            id: 2,
+
+            name: "Rum_8",
+
+            avatarName: "./assets/images/user/user02.png"
+
+        },
+
 
     ]
 
