d0ff2415a44755cbf7c2580f59158963fccd047e
Alexandr Ziborov <alexandr@getta.io>
2024-07-07 20:21:28 +0300

    Add user03

--- components/DatabaseHelper.tsx
+++ components/DatabaseHelper.tsx
@@ -2,10 +2,6 @@ import * as FileSystem from 'expo-file-system';
 
 import * as SQLite from 'expo-sqlite';
 
-import {Asset} from 'expo-asset';
-
-import {DATABASE_NAME} from '@/constants/DatabaseConst';
-
 const util = require('util');
 
 export class DatabaseHelper {
@@ -19,6 +15,8 @@ export class DatabaseHelper {
     private dbStep: number = 0;
 
     private readonly dbDir: string = "";
+
+    private db : any;
 
     constructor(SPADatabaseName) {
 
@@ -50,25 +48,24 @@ export class DatabaseHelper {
 
     async createSQLLite(sqlLiteName) {
 
-        const db = await SQLite.openDatabaseAsync(sqlLiteName);
+        this.db = await SQLite.openDatabaseAsync(sqlLiteName);
 
-        console.log(`db isPromise ${util.types.isPromise(db)}`);
+        console.log(`db isPromise ${util.types.isPromise(this.db)}`);
 
-        await db.execAsync(`
+        await this.db.execAsync(`
 PRAGMA journal_mode = WAL;
 CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY NOT NULL, avatar TEXT NOT NULL, user_name TEXT NOT NULL,  email TEXT NOT NULL,  home_page TEXT NOT NULL,  captcha TEXT NOT NULL,  text TEXT NOT NULL);
-INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUES (0, 'SPA01.png', 'Rum_8', 'Rum_8@gmail.com', 'https://www.linkedin.com/in/oleksandr-ziborov-10589192/', '12345', 'Everybody of us understood clear things: Eliminating external contradictions provides ample opportunities!' );
-INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUES (1, 'SPA02.png', 'Anonym', 'Anonym@gmail.com', 'https://www.linkedin.com/in/oleksandr-ziborov-10589192/', '67890', 'Suddenly, careful research of competitors, which represent a clear example of the continental European type of political culture, will be associatively distributed across industries.' );
-INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUES (2, 'SPA03.png', 'Oleks', 'oleksandrziborov@gmail.com', 'https://www.linkedin.com/in/oleksandr-ziborov-10589192/', '09876', 'Life is life.' );
+INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUES (0, 'user01.png', 'Rum_8', 'Rum_8@gmail.com', 'https://www.linkedin.com/in/oleksandr-ziborov-10589192/', '12345', 'Everybody of us understood clear things: Eliminating external contradictions provides ample opportunities!' );
+INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUES (1, 'user02.png', 'Anonym', 'Anonym@gmail.com', 'https://www.linkedin.com/in/oleksandr-ziborov-10589192/', '67890', 'Suddenly, careful research of competitors, which represent a clear example of the continental European type of political culture, will be associatively distributed across industries.' );
+INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUES (2, 'user03.png', 'Oleks', 'oleksandrziborov@gmail.com', 'https://www.linkedin.com/in/oleksandr-ziborov-10589192/', '09876', 'Life is life.' );
 
 `);
 
-        // `getFirstAsync()` is useful when you want to get a single row from the database.
-        const firstRow = await db.getFirstAsync('SELECT * FROM users');
+        const firstRow = await this.db.getFirstAsync('SELECT * FROM users');
 
-        console.log(firstRow.id, firstRow.avatar, firstRow.user_name);
+        console.log(firstRow.id, firstRow.avatar, firstRow.user_name, firstRow.email, firstRow.home_page, firstRow.captcha, firstRow.text);
 
-        const allRows = await db.getAllAsync('SELECT * FROM users');
+        const allRows = await this.db.getAllAsync('SELECT * FROM users');
 
         for (const row of allRows) {
 
@@ -76,7 +73,7 @@ INSERT INTO users (id, avatar, user_name, email, home_page, captcha, text) VALUE
 
         }
 
-        return db;
+        //return this.db;
 
     }
 
