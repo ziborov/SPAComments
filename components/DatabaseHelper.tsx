cb08cad3774e8e090a2352b8f20971c288885280
Alexandr Ziborov <alexandr@getta.io>
2024-07-07 16:07:50 +0300

    Create sqlLiteName

--- components/DatabaseHelper.tsx
+++ components/DatabaseHelper.tsx
@@ -2,31 +2,35 @@ import * as FileSystem from 'expo-file-system';
 
 import * as SQLite from 'expo-sqlite';
 
-import { Asset } from 'expo-asset';
+import {Asset} from 'expo-asset';
 
-import { DATABASE_NAME } from '@/constants/DatabaseConst';
+import {DATABASE_NAME} from '@/constants/DatabaseConst';
+
+const util = require('util');
 
 export class DatabaseHelper {
 
-    private SPADatabaseName: string = "";
+    private readonly SPADatabaseName: string = "";
 
     private openDatabaseStep: number = 0;
 
-    private db: any = null;
+    private dbName: string = "";
 
     private dbStep: number = 0;
 
-    private dbDir: string = "";
+    private readonly dbDir: string = "";
 
-    constructor (SPADatabaseName) {
+    constructor(SPADatabaseName) {
 
         this.SPADatabaseName = SPADatabaseName;
 
         this.openDatabaseStep = 0;
 
+        this.dbDir = FileSystem.cacheDirectory + 'db/';
+
     }
 
-    async getDirInfo () {
+    async getDirInfo() {
 
         let dirInfo = await FileSystem.getInfoAsync(this.dbDir);
 
@@ -34,25 +38,47 @@ export class DatabaseHelper {
 
     }
 
-    async createDir () {
+    async createDir() {
 
         console.log(`Db directory doesn't exist, ${this.dbDir} creating… `);
 
-        const createdDbDirResult = await FileSystem.makeDirectoryAsync(this.dbDir, { intermediates: true });
+        const createdDbDirResult = await FileSystem.makeDirectoryAsync(this.dbDir, {intermediates: true});
 
         return createdDbDirResult;
 
     }
 
-    openDatabase(openDatabaseStep)  {
+    async createSQLLite(sqlLiteName) {
+
+        //const db = await SQLite.openDatabaseAsync(sqlLiteName);
+        const db = await SQLite.openDatabaseAsync("sqlLiteName");
+
+        return db;
+
+    }
+
+    async dbUsersCreated(dbTable) {
+
+        const dbTableCreate = await dbTable.execAsync(`
+CREATE TABLE "users" ( "id" INTEGER, "avatar" TEXT, "email" TEXT, "home_page" TEXT, "captcha" TEXT, "text" TEXT, PRIMARY KEY("id" AUTOINCREMENT) )
+`);
+
+        return dbTableCreate;
+
+    }
+
+    async initTableDB(db) {
+
+
+    }
+
+    openDatabase(openDatabaseStep) {
 
         this.openDatabaseStep = openDatabaseStep;
 
         switch (this.openDatabaseStep) {
 
-            case 0:
-
-                this.dbDir = FileSystem.cacheDirectory + 'db/';
+            case 0: //Db directory doesn't exist, ${this.dbDir} creating…
 
                 console.log(`dbDir: ${this.dbDir}`);
 
@@ -64,18 +90,65 @@ export class DatabaseHelper {
 
                     if (!dirInfo.exists) {
 
-                        this.createDir().then(r => console.log(`Create dir result ${r}`));
+                        this.createDir().then(r => console.log(`Dir created!!`));
 
                     } else {
 
                         this.dbStep = 1;
+
+                        this.dbName = this.dbDir + this.SPADatabaseName;
 
                     }
 
                 })
 
                 break;
-            
+
+            case 1:
+
+                //console.log(`Create SQLite db ${this.dbName}`);
+                console.log(`Create SQLite: sqlLiteName`);
+
+                const db = this.createSQLLite(this.dbName);
+
+                console.log(`db isPromise ${util.types.isPromise(db)}`);
+
+                if (util.types.isPromise(db)) {
+
+                    db.then(dbBase => {
+
+                        const dbUsers = this.dbUsersCreated(dbBase);
+
+                        console.log(`dbUsers isPromise ${util.types.isPromise(dbUsers)}`);
+
+                        // if (util.types.isPromise(dbUsers)) {
+                        //
+                        //     this.dbUsersCreate(dbUsers).then(res => {
+                        //
+                        //         console.log(``)
+                        //
+                        //     })
+                        //
+                        // }
+
+                    })
+
+                }
+
+
+                // this.initTableDB(db).then(r => {
+                //
+                //     console.log(`initTableDB worked`);
+                //
+                //     this.dbStep = 2;
+                //
+                //     }
+                // );
+
+                this.dbStep = 2;
+
+                break;
+
             // default:
             //
             //     console.log(`Error! openDatabaseStep: ${this.openDatabaseStep}`);
