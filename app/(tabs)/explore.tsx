89833516d00b5f64fbc5b8085795f6fdf8090045
Alexandr Ziborov <alexandr@getta.io>
2024-07-08 10:10:43 +0300

    Fix onPressCreateUser

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -167,7 +167,7 @@ export default function TabTwoScreen() {
 
                     <TouchableOpacity
 
-                        onPress={() => onPressCreateUser}
+                        onPress={onPressCreateUser}
 
                         >
 
